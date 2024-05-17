72c3e08f9021eeec02a00ab97b63aea013c25c17
DIM <4ebotarov_dim@inbox.ru>
2024-05-17 22:43:36 +0300

    фильтрация рейтинга

--- STL_list.cpp
+++ STL_list.cpp
@@ -5,8 +5,20 @@ using namespace std;
 int main()
 {
 	list<int> allPleyers = { 2,9,6,7,3,1,4,8,3,2,9 }; // все игроки { список рейтингов игроков}
-	list<int> beginners;//список начинающие
-	list<int>profi; //список профи
+	list<int> beginners;//список начинающие //reting 1-5
+	list<int>profi; //список профи  //reting 6-10
+
+	for (list<int>::iterator it = allPleyers.begin(); it != allPleyers.end(); it++) {
+		int rating = *it;
+		if (rating >= 1 && rating <= 5) {
+			beginners.push_back(rating);
+		}
+		else if (rating >= 6 && rating <= 10) {
+			profi.push_back(rating);
+		}
+	}
+
+
 
 	cin.get();
 }
