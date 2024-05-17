784027370a5eb2cb19761ffedfe6eda779e92428
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-17 20:34:30 +0300

    иниц списка

--- STL_list.cpp
+++ STL_list.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 int main()
 {
+	list<int> myList; //  лист инициализация списка
+	myList.push_back(10); //добавить в конец списка
+	myList.push_back(20); 
+	myList.push_front(30)//добавить в начало списка
+
 
 
    cin.get();
