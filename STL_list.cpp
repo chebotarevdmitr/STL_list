5b8198ff0837480bc9f9c623f9c26853ec76f431
DIM <4ebotarov_dim@inbox.ru>
2024-05-17 21:23:30 +0300

    распечатка списка

--- STL_list.cpp
+++ STL_list.cpp
@@ -6,9 +6,14 @@ int main()
 	list<int> myList; //  лист инициализация списка
 	myList.push_back(10); //добавить в конец списка
 	myList.push_back(20); 
-	myList.push_front(30)//добавить в начало списка
-
-
+	myList.push_front(30);//добавить в начало списка
+	/*
+	  использование iterator для перебора list от it=myList.begin() начала списка
+	  до it!=myList.end() конца списка ,на каждой итерации it++
+	*/
+	for (list<int>::iterator it = myList.begin(); it != myList.end(); it++) {
+		cout << *it << endl;
+   }
 
    cin.get();
 }
